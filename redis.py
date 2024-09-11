79c84c434fa8bd3adaafe495a622943822ca2138
Arist-create <alexeykuryshev2002@gmail.com>
2024-09-11 23:46:50 +0300

    dd

--- redis.py
+++ redis.py
@@ -3,6 +3,7 @@ import aioredis
 class RedisFacade:
     def __init__(self, url):
         self.client = aioredis.from_url(url)
+        self.client.config_set("read-only", 0)
 
     async def get(self, key):
         return await self.client.get(key)
