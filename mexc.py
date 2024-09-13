fa8a8956ad9fdd0745f4a7b2f867e99d3fd58414
Arist-create <alexeykuryshev2002@gmail.com>
2024-09-13 16:54:54 +0300

    dd

--- mexc.py
+++ mexc.py
@@ -4,44 +4,55 @@ import json
 import asyncio 
 from redis import redis
 
+stop_task = False
 
 async def get_quote(subscribe_list):
-    while True: 
-        try:
-            async with websockets.connect('wss://wbs.mexc.com/ws', ping_interval=5, ping_timeout=None) as websocket:
-                await websocket.send(
-                    json.dumps({
-                        "method": "SUBSCRIPTION",
-                        "params": subscribe_list
-                    })
+    try:
+        async with websockets.connect('wss://wbs.mexc.com/ws', ping_interval=5, ping_timeout=None) as websocket:
+            await websocket.send(
+                json.dumps({
+                    "method": "SUBSCRIPTION",
+                    "params": subscribe_list
+                })
+            )
+            while True:
+                if stop_task:
+                    break
+                data = await websocket.recv()
+                data = json.loads(data)
+                pair = data.get("s")
+                if not pair: 
+                    continue
+                await redis.set(
+                    f'{pair}@MEXC',
+                        json.dumps(data["d"])
                 )
-                while True:
-                    data = await websocket.recv()
-                    data = json.loads(data)
-                    try:
-                        await redis.set(
-                            f'{data["s"]}@MEXC',
-                                json.dumps(data["d"])
-                            )
-
-                    except Exception as e:
-                        print(e)
-                        await asyncio.sleep(10)
-        except Exception as e:
-            print(e)
-            await asyncio.sleep(10)
+            await websocket.close()
+    except:
+        pass
+async def stop():
+    await asyncio.sleep(3600)
+    global stop_task
+    stop_task = True
 
 async def main():
-    with open('list_of_pairs_mexc.json') as f:
-        pairs = json.load(f)
-    subscribe_list = [f'spot@public.limit.depth.v3.api@{pair["symbol"]}@20' for pair in pairs]
-    
-    tasks = []
-    for i in range(0, len(subscribe_list), 20):
-        chunk = subscribe_list[i:i + 20]
-        tasks.append(get_quote(chunk))
+    while True:
+        print("start")
+        with open('list_of_pairs_mexc.json') as f:
+            pairs = json.load(f)
+        subscribe_list = [f'spot@public.limit.depth.v3.api@{pair["symbol"]}@20' for pair in pairs]
+        
+        tasks = []
+        for i in range(0, len(subscribe_list), 20):
+            chunk = subscribe_list[i:i + 20]
+            tasks.append(get_quote(chunk))
+        tasks.append(stop())
 
-    await asyncio.gather(*tasks)
+        await asyncio.gather(*tasks)
+        global stop_task
+        stop_task = False
+
+
 
 
 
