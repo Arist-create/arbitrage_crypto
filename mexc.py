d3b349fe4a04bad3970ce3d2d9b1cafef635e3b5
Arist-create <alexeykuryshev2002@gmail.com>
2024-09-30 23:54:52 +0300

    first commit

--- mexc.py
+++ mexc.py
@@ -17,9 +17,7 @@ async def get_quote(subscribe_list):
                         "params": subscribe_list
                     })
                 )
-                while True:
-                    if stop_task:
-                        break
+                while not stop_event.is_set():
                     data = await websocket.recv()
                     data = json.loads(data)
                     pair = data.get("s")
